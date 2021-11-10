24229366eb919c1d3d4115347a7937a6dab1f2dd
mateusPrintf <mateus.meneses@hotmail.com.br>
2021-11-10 10:30:01 -0300

    Alterando de Stateless p/ Statefull e fazendo transição de estados.

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -2,12 +2,13 @@
 
 main() => runApp(PerguntaApp());
 
- class PerguntaApp extends StatelessWidget{
-
+ class PerguntaAppState extends State<PerguntaApp> {
    var perguntaSelecionada = 0;
 
    void responder(){
-     perguntaSelecionada++;
+     setState(() {
+       perguntaSelecionada++;
+     });
    }
 
    @override
@@ -46,5 +47,14 @@ main() => runApp(PerguntaApp());
        ),
      );
    }
+
+ }
+
+ class PerguntaApp extends StatefulWidget{
+
+  PerguntaAppState createState(){
+    return PerguntaAppState();
+   }
+
  }
 
